7a41bd997a991e5fbc77a03dc8cc309fa3643398
Arthur M <143713077+murraythur@users.noreply.github.com>
2023-12-23 21:43:10 -0300

    collect_asm.sh

---
+++ raspberry_cfg/collect_asm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CORE_LOC="$SCRIPT_LOC/../../core-legacy"
+
+if [[ "$(uname -a)" =~ Linux ]]; then
+	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
+		OSMAND_BUILD_CPU_CORES_NUM=`nproc`
+	fi
+fi
+
+function copyLibs {
+	if [ -d "$CORE_LOC/binaries/$1/$2" ]; then 
+		echo "Copy binaries $1 $2";
+		mkdir -p "$SCRIPT_LOC"/src/main/resources/
+		cp "$CORE_LOC"/binaries/$1/$2/Release/libosmand.so "$SCRIPT_LOC"/src/main/resources/osmand-$1-$3.lib
+	fi
+}
+
+function compile {
+	"$CORE_LOC/externals/configure.sh"
+	ARCH=$1
+	#COMPILER=gcc
+	COMPILER=clang
+	if [ ! -d "$CORE_LOC/targets/$ARCH-linux-$COMPILER-$ARCH-linux-$COMPILER-release.baked" ]; then 
+		"$CORE_LOC/targets/$ARCH-linux-$COMPILER.sh" release
+	fi
+	(cd "$CORE_LOC/targets/$ARCH-linux-$COMPILER-$ARCH-linux-$COMPILER-release.baked" && make -j$OSMAND_BUILD_CPU_CORES_NUM)
+
+}
+
+compile amd64
+#compile i686
+copyLibs linux amd64 amd64 so
+#copyLibs linux i686 x86 so
